6b2229d76667e426255a00e45b56c73c82a7937e
darrenZzz <2901359206@qq.com>
2021-04-06 11:50:08 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity{
+    TextView out;
+    EditText inp;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        out = findViewById(R.id.textView2);
+        inp = findViewById(R.id.inp);
+    }
+
+    public void btn(View v){
+        String str = inp.getText().toString();
+        if (str != null&& str.length()>0){
+            Double a = Double.valueOf(str).doubleValue();
+            a = 32 + a * 1.8;
+            out.setText("结果为："+a+"华氏度");
+        }else{
+            out.setText("请输入你想转换的摄氏度");
+        }
+    }
+
+
+}
